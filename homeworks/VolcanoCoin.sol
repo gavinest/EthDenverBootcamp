b6771c2625d19303137ecc9c41aaf8aa93a4f50b
Gavin Estenssoro <gavin.estenssoro@pinnacol.com>
2022-10-26 20:31:59 -0600

    import openzeppelin

--- homeworks/VolcanoCoin.sol
+++ homeworks/VolcanoCoin.sol
@@ -2,8 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-contract VolcanoCoin {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.7.3/contracts/access/Ownable.sol";
 
+contract VolcanoCoin is Ownable {
     address owner;
     uint256 public supply = 10000;
     mapping(address => uint256) userBalances;
@@ -15,41 +16,40 @@ contract VolcanoCoin {
         address recipient;
     }
 
-
-    // modifiers
-    modifier onlyOwner {
-        require (msg.sender == owner);
-        _;
-    }
-
     // events
     event supplyChangeEvent(uint256);
     event transferEvent(uint256, address indexed);
 
-    constructor () {
+    constructor() {
         owner = msg.sender;
         userBalances[msg.sender] = supply;
     }
 
-    function increaseSupply () public onlyOwner {
+    function increaseSupply() public onlyOwner {
         uint256 newSupply = supply + 1000;
         supply = newSupply;
         emit supplyChangeEvent(newSupply);
     }
 
-    function userBalance(address _user) public view returns(uint256) {
+    function userBalance(address _user) public view returns (uint256) {
         // get a user's balance
         return userBalances[_user];
     }
 
-    function transfer(uint256 _amount, address payable _recipient) public payable {
+    function transfer(uint256 _amount, address payable _recipient)
+        public
+        payable
+    {
         // transfer an amount to a specified address
 
         require(_amount > 0, "transfer amount must be greater than 0");
-        require(msg.value >= _amount, "the msg.value must be greater than requested amount");
+        require(
+            msg.value >= _amount,
+            "the msg.value must be greater than requested amount"
+        );
 
         // send using recommented method https://solidity-by-example.org/sending-ether/
-        (bool sent,) = _recipient.call{value: _amount}("");
+        (bool sent, ) = _recipient.call{value: _amount}("");
         require(sent, "Failed to send either");
 
         emit transferEvent(_amount, _recipient);
